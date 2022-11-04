8a6ed855467a37c9e2ce1ef51215d1b847af8383
Anthony Joseph Pompetti <ajpompet@biomix.dbi.udel.edu>
2022-11-04 13:06:09 -0400

    added the shell script to allow for sbatch submission

---
+++ nextflow.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --output=./slurm-out/nextflowmain-%j.out
+#SBATCH --job-name=nf_main
+#SBATCH --mem=32000
+#SBATCH -c 1
+mkdir -p slurm-out
+
+rm -rf work/; rm .nextflow.log*; rm -rf results/
+find ./slurm-out/ -type f -not -name "*-${SLURM_JOBID}.out" -delete
+nextflow run Duncan_Lab_Pipeline --genome 'GRCm39' --rmrRNA
